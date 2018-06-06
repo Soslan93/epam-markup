8e6838181834bd6707797ad287ccd3e356cd3e68
Soslan <kulumbekov1807@gmail.com>
2018-06-06 21:56:19 +0300

    bem practice

--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ import sum from './additional';
 
 console.log(sum('Hello'));
 
-import './css/style.css';
+import './css/body.css';
+import './css/menu.css';
+import './css/portfolio.css';
+import './css/main.css';
+import './css/footer.css';
